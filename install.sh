b6487888a4086e06fa1d22a79025a00134d014cf
Denis Koltsov <iam@mistadikay.com>
2016-06-30 18:56:36 +0700

    :heavy_plus_sign: add install script

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#
+# Initial installation
+
+# set hostname
+scutil --set HostName mistadikay
+
+# install fishmarks
+curl -L https://github.com/techwizrd/fishmarks/raw/master/install.fish | fish
+
+# set symlinks from .dotfiles
+./sync.sh
