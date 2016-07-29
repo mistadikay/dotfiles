905df47cb14628f30e50e0032d6b37f7848223f7
Denis Koltsov <iam@mistadikay.com>
2016-07-29 12:40:24 +0700

    :fish: fish: remove greeting

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,6 @@
+# remove greeting
+set fish_greeting
+
 # Set aliases
 alias c "clear"
 alias b "babel-node"
@@ -15,6 +18,7 @@ set --export PATH /usr/local/sbin $PATH
 # https://github.com/rgcr/m-cli
 set --export PATH /usr/local/m-cli $PATH
 
+# fisher stuff
 set fisher_home ~/.local/share/fisherman
 set fisher_config ~/.config/fisherman
 source $fisher_home/config.fish
