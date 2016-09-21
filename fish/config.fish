7d872a2a67102972b5a3b80f7f93c6856a1a0749
Denis Koltsov <iam@mistadikay.com>
2016-09-21 22:51:54 +0700

    :heavy_minus_sign: fisher: remove unused stuff, use fnm instead of nvm

--- fish/config.fish
+++ fish/config.fish
@@ -23,14 +23,6 @@ set --export GO15VENDOREXPERIMENT 1
 # for homebrew
 set --export PATH /usr/local/sbin $PATH
 
-# https://github.com/rgcr/m-cli
-set --export PATH /usr/local/m-cli $PATH
-
-# fisher stuff
-set fisher_home ~/.local/share/fisherman
-set fisher_config ~/.config/fisherman
-source $fisher_home/config.fish
-
 # Load fishmarks (http://github.com/techwizrd/fishmarks)
 . $HOME/.fishmarks/marks.fish
 
