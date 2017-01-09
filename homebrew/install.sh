1e84256fc86fadc083c36f7f7a8902ca4d777a1c
Denis Koltsov <iam@mistadikay.com>
2017-01-09 17:12:22 +0100

    :heavy_check_mark: fix homebrew install script

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -7,23 +7,7 @@ brew="$(brew --prefix)/bin/brew"
 
 echo "Installing Homebrew stuff"
 echo "│"
-echo "└── Enabling taps..."
-for TAP in `cat $dotfiles/homebrew/brew-sync.taps`; do
-	$brew tap ${TAP} >/dev/null
-done
-
-echo "│"
-echo "└── Installing missing packages..."
-for PACKAGE in `cat $dotfiles/homebrew/brew-sync.installed`; do
-	$brew leaves ${PACKAGE} >/dev/null
-	[ "$?" != "0" ] && $brew install ${PACKAGE}
-done
-
-echo "│"
-echo "└── Installing missing casks..."
-for CASK in `cat $dotfiles/homebrew/brew-sync.casks`; do
-	$brew cask list -1 ${CASK} >/dev/null
-	[ "$?" != "0" ] && $brew cask install ${CASK}
-done
+echo "└── Restoring from bundle..."
+$brew bundle
 
 printf "\n"
