70a1efd0681faf9997fcb0d4ebb4348bfa4a10af
Denis Koltsov <iam@mistadikay.com>
2018-10-23 09:28:48 +0200

    :bug: Fix deprecated fisher command in up.fish

--- fish/functions/up.fish
+++ fish/functions/up.fish
@@ -1,47 +1,47 @@
 # update dependencies
 function up
-	switch (uname)
-	case Linux
-		if test /etc/redhat-release
-			sudo dnf clean all
-			sudo dnf upgrade --refresh
-			sudo yum update
-		else
-			sudo apt update
-			sudo apt full-upgrade
-		end
-	case '*'
-		# show mac updates
-		softwareupdate -l
+    switch (uname)
+        case Linux
+            if test /etc/redhat-release
+                sudo dnf clean all
+                sudo dnf upgrade --refresh
+                sudo yum update
+            else
+                sudo apt update
+                sudo apt full-upgrade
+            end
+        case '*'
+            # show mac updates
+            softwareupdate -l
 
-		# upgrade mac apps
-		mas upgrade
+            # upgrade mac apps
+            mas upgrade
 
-		# brew
-		brew update --rebase
-		brew upgrade
-		brew cleanup -s
-		brew prune
+            # brew
+            brew update --rebase
+            brew upgrade
+            brew cleanup -s
+            brew prune
 
-		# https://github.com/rgcr/m-cli
-		m --update
-	end
+            # https://github.com/rgcr/m-cli
+            m --update
+    end
 
-	# https://github.com/fish-shell/fish-shell/blob/master/doc_src/fish_update_completions.txt
-	fish_update_completions
+    # https://github.com/fish-shell/fish-shell/blob/master/doc_src/fish_update_completions.txt
+    fish_update_completions
 
-	# update fishmarks (https://github.com/techwizrd/fishmarks)
-	cd ~/.fishmarks
-	git fetch --all
-	git reset --hard origin/master
+    # update fishmarks (https://github.com/techwizrd/fishmarks)
+    cd ~/.fishmarks
+    git fetch --all
+    git reset --hard origin/master
 
-	# update fisher
-	fisher update
+    # update fisher
+    fisher self-update
 
-	# update Rust toolchains and rustup
-	rustup update
+    # update Rust toolchains and rustup
+    rustup update
 
-	# update npm and global npm packages
-	npm i npm -g
-	npm update -g
+    # update npm and global npm packages
+    npm i npm -g
+    npm update -g
 end
