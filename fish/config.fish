2da84d9e8a9708a371191c71625ca1a181b85e0c
Denis Koltsov <denis.koltsov@paradoxinteractive.com>
2020-04-03 12:32:26 +0200

    Replace pyenv with anaconda

--- fish/config.fish
+++ fish/config.fish
@@ -42,6 +42,9 @@ set --export PATH $HOME/.cargo/bin $PATH
 # Brew stuff
 set --export PATH /usr/local/sbin $PATH
 
+# Anaconda
+set --export PATH $HOME/opt/anaconda3/bin $PATH
+
 # pgloader (built from source because see link below)
 # https://github.com/Homebrew/homebrew-core/issues/31591
 set --export PATH /Applications/pgloader/build/bin $PATH
@@ -52,12 +55,15 @@ set --export PATH /Applications/pgloader/build/bin $PATH
 test -e {$HOME}/.iterm2_shell_integration.fish
 and source {$HOME}/.iterm2_shell_integration.fish
 
-# pyenv
-status --is-interactive
-and source (pyenv init -|psub)
-
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f $HOME/yes/google-cloud-sdk/path.fish.inc ]
     echo "stuff"
     . $HOME/yes/google-cloud-sdk/path.fish.inc
 end
+set -g fish_user_paths "/usr/local/opt/tomcat@8/bin" $fish_user_paths
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+eval /Users/denis.koltsov/opt/anaconda3/bin/conda "shell.fish" "hook" $argv | source
+# <<< conda initialize <<<
+
