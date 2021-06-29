2e0801218fa7e55dd5070191cef5391916f74a7e
Denis Koltsov <iam@mistadikay.com>
2021-06-29 10:07:50 +0200

    Remove google cloud SDK from fish config

--- fish/config.fish
+++ fish/config.fish
@@ -2,16 +2,16 @@
 set fish_greeting
 
 # Set aliases
-alias k "kubectl"
-alias c "clear"
-alias b "babel-node"
-alias fi "fisher"
-alias tc "itermocil"
+alias k kubectl
+alias c clear
+alias b babel-node
+alias fi fisher
+alias tc itermocil
 alias brc "brew cask"
 alias brci "brew cask install"
 alias brcu "brew cask uninstall"
-alias y "yarn"
-alias r "yarn"
+alias y yarn
+alias r yarn
 alias gcl "git clone"
 alias gs "git status"
 alias gp "git push"
@@ -32,9 +32,6 @@ set --export GOPATH $HOME/go
 set --export PATH $GOPATH/bin $PATH
 set --export PATH $GOPATH/go_appengine $PATH
 set --export PATH $HOME/.local/bin $PATH
-
-# Google Cloud SDK
-set --export PATH $HOME/google-cloud-sdk/bin $PATH
 
 # Rust
 set --export PATH $HOME/.cargo/bin $PATH
@@ -59,9 +56,4 @@ set --export PATH /Applications/pgloader/build/bin $PATH
 test -e {$HOME}/.iterm2_shell_integration.fish
 and source {$HOME}/.iterm2_shell_integration.fish
 
-# The next line updates PATH for the Google Cloud SDK.
-if [ -f $HOME/yes/google-cloud-sdk/path.fish.inc ]
-    echo "stuff"
-    . $HOME/yes/google-cloud-sdk/path.fish.inc
-end
 set -g fish_user_paths "/usr/local/opt/tomcat@8/bin" $fish_user_paths
