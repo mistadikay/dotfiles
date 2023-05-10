df48907d859c58cfbc2f3a129b68dce007d8648a
Denis Koltsov <denis.koltsov@paradoxinteractive.com>
2023-05-10 10:57:45 +0200

    Cleanup a list of macos apps

--- macos/install.sh
+++ macos/install.sh
@@ -1,20 +1,9 @@
 #!/usr/bin/env bash
 
 echo "└── Tweaking macOS settings"
-scutil --set HostName mistadikay
+scutil --set HostName deniskoltsov
 
 echo "└── Installing apps from Mac App Store"
-mas lucky cleanmymacx
 mas lucky copyclip
 mas lucky daisydisk
 mas lucky gemini
-mas lucky kindle
-mas lucky mactracker
-mas lucky nordvpn
-mas lucky omnifocus3
-mas lucky owly
-mas lucky slack
-mas lucky telegram
-mas lucky tweetbot3
-mas lucky wallcat
-mas install 497799835 #xcode
