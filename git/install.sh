016dc9b8e9a65589773e198ce6f91cf37ce3afe4
Denis Koltsov <iam@mistadikay.com>
2024-04-02 15:17:27 +0200

    Make gpg path and signingkey in .gitconfig depend on the machine

--- git/install.sh
+++ git/install.sh
@@ -4,11 +4,60 @@
 
 src="$HOME/.dotfiles/git"
 dest=$HOME
-files=(gitconfig gitignore)
+files=(gitignore)
 
 echo "Installing Git dotfiles"
 
-echo "└── Linking to $dest:"
+# Handle .gitconfig with OS-specific gpg program path and signingkey
+echo "└── Customizing and linking .gitconfig to $dest:"
+
+
+os=$(uname)
+case "$os" in 
+  "Linux")
+    gpg_program="/usr/bin/gpg"
+    ;;
+  "Darwin")
+    gpg_program="/opt/homebrew/bin/gpg"
+    ;;
+  *)
+    echo "Unknown os. Cannot configure GPG program."
+    exit 1
+    ;;
+esac
+
+hostname=$(hostname)
+case "$hostname" in
+    "deniskoltsov")
+        signingkey="3C9D9D280118ED0A"
+        ;;
+    "Deniss-MBP")
+        signingkey="3C9D9D280118ED0A"
+        ;;
+    "linux-hostname")
+        signingkey="EE5EC460D5BF2118"
+        ;;
+    *)
+        echo "Unknown machine. Cannot configure GPG signing key automatically."
+        exit 1
+        ;;
+esac
+
+
+# Copy .gitconfig to temporary file and customize
+tmp_gitconfig=$(mktemp)
+cp "$src/gitconfig" "$tmp_gitconfig"
+
+# Append the gpg program and signingkey configurations
+echo -e "\n[gpg]\n    program = $gpg_program" >> "$tmp_gitconfig"
+echo -e "[user]\n    signingkey = $signingkey" >> "$tmp_gitconfig"
+
+# Replace existing .gitconfig with customized version
+rm -f $dest/.gitconfig
+ln -s $tmp_gitconfig $dest/.gitconfig
+
+# Link other files as before
+echo "└── Linking other Git files to $dest:"
 for file in ${files[@]}; do
   echo "    └── $file"
 
