8f1cc9b89f31f3f6186619f8ecf8e4aedc9d56c6
Denis Koltsov <denis.koltsov@paradoxinteractive.com>
2024-08-15 11:50:01 +0200

    Fix incorrect config value

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -6,7 +6,7 @@ config = {
     automatically_reload_config = true,
     enable_tab_bar = false,
     window_close_confirmation = "NeverPrompt", 
-    window_decoration = "RESIZE",
+    window_decorations = "RESIZE",
     default_cursor_style = "BlinkingBar",
     color_scheme = "zenwritten_dark",
     font = wezterm.font("JetBrains Mono", { weight = "Regular" }),
