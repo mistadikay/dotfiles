b3ecfa293c321558d6f1d29a80e8c93cdb21cc9b
Denis Koltsov <iam@mistadikay.com>
2024-10-04 15:41:02 +0200

    Remove kafka from fish config

--- fish/config.fish
+++ fish/config.fish
@@ -60,10 +60,6 @@ set --export PATH /usr/lib/docker/cli-plugins $PATH
 # Rust
 set --export PATH $HOME/.cargo/bin $PATH
 
-# Kafka
-set --export KAFKA_HOME /Applications/kafka_2.13-2.8.0/
-set --export PATH $KAFKA_HOME/bin $PATH
-
 # Brew stuff
 set -U fish_user_paths /opt/homebrew/bin $fish_user_paths
 set --export PATH /usr/local/sbin $PATH
