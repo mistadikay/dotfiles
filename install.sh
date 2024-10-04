aaaffed449e3dea43de30bb95693182472c55b49
Denis Koltsov <iam@mistadikay.com>
2024-10-04 15:42:36 +0200

    Reorder install scrip in main install.sh

--- install.sh
+++ install.sh
@@ -7,17 +7,16 @@
 dotfiles="$HOME/.dotfiles"
 
 # Install, setup and symlink everything
-bash $dotfiles/asdf/install.sh
-bash $dotfiles/fish/install.sh
 bash $dotfiles/homebrew/install.sh
+bash $dotfiles/fish/install.sh
 bash $dotfiles/macos/install.sh
 bash $dotfiles/bash/install.sh
 bash $dotfiles/git/install.sh
 bash $dotfiles/npm/install.sh
-bash $dotfiles/vim/install.sh
-
-# Synchronize
-bash $dotfiles/sync.sh
+bash $dotfiles/asdf/install.sh
+bash $dotfiles/psql/install.sh
+bash $dotfiles/tmux/install.sh
+bash $dotfiles/wezterm/install.sh
 
 echo "\(• ◡ •)/      dotfiles installed!     ᕕ( ᐛ )ᕗ"
 printf "\n"
