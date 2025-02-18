641748684c7c4142ebfd7833269542e56c76bfdc
Denis Koltsov <iam@mistadikay.com>
2025-02-18 11:51:19 +0100

    Add asdf configuration to fish

--- fish/config.fish
+++ fish/config.fish
@@ -22,6 +22,17 @@ alias ls='eza'
 alias ll='eza -alh'
 alias tree='eza --tree'
 
+# ASDF configuration code
+if test -z $ASDF_DATA_DIR
+    set _asdf_shims "$HOME/.asdf/shims"
+else
+    set _asdf_shims "$ASDF_DATA_DIR/shims"
+end
+if not contains $_asdf_shims $PATH
+    set -gx --prepend PATH $_asdf_shims
+end
+set --erase _asdf_shims
+
 if test -f /etc/debian_version
     alias cat='batcat'
 else
